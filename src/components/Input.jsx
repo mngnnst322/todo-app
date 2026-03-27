7a8815cf93d27ea11cd83766c2f7b1bd5174d4ee
mngnnst322 <myangannast.d209@gmail.com>
2026-03-27 12:58:28 +0800

    todo-app2

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-export const Input = () => {
+export const Input = ({ setValue }) => {
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <input
+      onChange={handleChange}
       className="rounded-md h-[40px] border w-70 px-4 py-2 box-border"
       placeholder="Add a new task..."
     />
